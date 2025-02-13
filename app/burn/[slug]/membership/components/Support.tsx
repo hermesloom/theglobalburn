0a9d3f56d2561ef03ae5546babcc209d12bc5eca
henophilia <tech@henophilia.org>
2025-02-13 19:39:17 +0100

    fix: change support email address to tech@theborderland.se

--- app/burn/[slug]/membership/components/Support.tsx
+++ app/burn/[slug]/membership/components/Support.tsx
@@ -10,10 +10,7 @@ export default function InvitePlusOne() {
       <Heading className="mt-12">Support</Heading>
       <p>
         In case of any questions or issues, please contact{" "}
-        <Link href="mailto:memberships@theborderland.se">
-          memberships@theborderland.se
-        </Link>
-        .
+        <Link href="mailto:tech@theborderland.se">tech@theborderland.se</Link>.
       </p>
     </>
   );
