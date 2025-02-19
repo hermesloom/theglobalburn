003d1412a3bae349c28de5976a4d0f6f8dd05e54
henophilia <tech@henophilia.org>
2025-02-19 17:13:29 +0100

    fix: make notes on initial page clearer

--- app/page.tsx
+++ app/page.tsx
@@ -6,30 +6,36 @@ import { useRouter } from "next/navigation";
 import ActionButton from "@/app/_components/ActionButton";
 import { apiPost } from "@/app/_components/api";
 import { useSession } from "@/app/_components/SessionContext";
+import { BurnStage } from "@/utils/types";
 
 const DEFAULT_PROJECT_SLUG = "the-borderland-2025";
 
 export default function Home() {
   const { profile, reloadProfile } = useSession();
   const router = useRouter();
+  const project = profile?.projects.find(
+    (p) => p.slug === DEFAULT_PROJECT_SLUG,
+  );
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="text-8xl mb-4">❤️‍🔥</div>
-      {profile?.projects.find((p) => p.slug === DEFAULT_PROJECT_SLUG) ? (
+      {project ? (
         <Button
           color="primary"
           onPress={() =>
             router.push(`/burn/${DEFAULT_PROJECT_SLUG}/membership`)
           }
         >
-          Go to my membership
+          {project.burn_config.current_stage === BurnStage.LotteryOpen
+            ? "Click here to go to the lottery"
+            : "Manage my membership"}
         </Button>
       ) : (
         <ActionButton
           action={{
             key: "join",
-            label: "Join now!",
+            label: "Please click here!",
             onClick: async () => {
               await apiPost(`/burn/${DEFAULT_PROJECT_SLUG}/join`);
               await reloadProfile();
