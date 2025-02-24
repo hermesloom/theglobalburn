cfff13f44090cbd4e1f6757599f38cabc366c04a
rhlie <runar.lie@gmail.com>
2025-02-24 17:47:31 +0000

    Adding burner questions sql

---
+++ supabase/questions.sql
@@ -0,0 +1,84 @@
+INSERT INTO "public"."questions" ("id", "created_at", "project_id", "question_id", "question_text", "question_type", "question_options", "question_description", "question_order") VALUES ('04c2ede8-3307-4213-902d-d61501718a79', '2025-02-24 17:30:43.92876+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'when-do-you-plan-to-arrive', 'When do you plan to arrive?', 'radio', 'Arriving earlier to build stuff and help out (like a boss)
+Arriving between Sunday and Monday (like a normal person)
+Arriving Tuesday or later (like a tourist)
+Only there for the weekend (what am I even doing here?)
+', ' ', '1100'), ('069401c8-fb23-4d79-b7fb-bed0c83b459d', '2025-02-24 17:41:56.236142+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'are-you-aware-that-the-borderland-in-alversjo-is-surrounded-by-a-nature-reserve-and-that-we-should-not-camp-make-fires-or-build-things-there', 'Are you aware that The Borderland in Alversjö is surrounded by a nature reserve, and that we should not camp, make fires or build things there?', 'radio', 'Yes, and I know about the responsibilities that come with it :)	
+I knew about the reserve, but not about the rules. I will make sure I know about it!
+No I didn't know that, but I will read the Survival Guide and educate myself!', ' ', '2700'), ('36714637-9e84-4f84-9f32-8954800214d6', '2025-02-24 17:31:46.390947+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'how-many-times-have-you-been-to-the-borderland-before', 'How many times have you been to The Borderland before?', 'text', ' ', ' ', '1400'), ('42c4213b-1027-41ea-afb8-7ffe6b563d29', '2025-02-24 17:30:13.772905+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'in-which-country-do-you-currently-reside', 'In which country do you currently reside?', 'text', ' ', ' ', '1000'), ('63fe74e6-c89d-4af3-acfb-971440348b53', '2025-02-24 17:37:18.214627+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'so-you-want-to-start-a-sound-camp-to-bring-that-old-school-disco-back-to-the-borderland-what-do-you-do', 'So, you want to start a sound camp to bring that old school disco back to the Borderland. What do you do?', 'checkbox', 'I go to Borderlands Discord channel and propose the sound-camp as a part of the co-created mish-mash	
+Go to Borderlands Facebook page to see if someone wants to help out	
+Check in the Survival Guide to see if there are any pointers there	
+I put a big ass speaker in the middle of Kidsville and scream “RADICAL SELF EXPRESSION”	
+Hire famous DJs and make a fancy PR campaign to get the crowd hyped
+', ' ', '2300'), ('6d66ea4b-ce73-4530-b05c-5afa4c834f35', '2025-02-24 17:32:35.910801+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'are-you-aware-of-the-fact-that-youre-heading-straight-into-a-radical-experiment-in-community-co-creation-and-creativity', 'Are you aware of the fact that  you're heading straight into a radical experiment in community, co-creation and creativity?', 'radio', 'Yes, there are no spectators
+Love that stuff, see you on Discord! (link?)
+No, but I'll read about it in the survival guide right away (link?)
+', ' ', '1600'), ('705d4c82-53f9-4eda-b072-fa2b8660ec91', '2025-02-24 17:43:35.321081+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'are-you-aware-of-the-fact-that-this-questionnaire-is-but-a-failed-attempt-at-explaining-the-beautiful-chaos-that-is-the-borderland', 'Are you aware of the fact that this questionnaire is but a failed attempt at explaining the beautiful chaos that is the Borderland?', 'checkbox', 'Yes, Borderland is a fully decentralized madness with no formal authority	
+No, you’re doing great! Hooray for you!	
+Yes, it’s impossible to briefly describe an event that is always in the process, and never fixed.
+Yes, and I’m super tired of it. Let’s meet on Discord to organize stuff instead!', '  ', '3000'), ('75f7353b-1cd9-4509-b7ab-bdc3a45d9147', '2025-02-24 17:36:17.239668+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'do-you-have-a-dream-for-this-years-borderland', 'Do you have a dream for this year's Borderland?', 'radio', 'Yes, I have my own dream and I have already written a description and a budget
+Yes, but I'm still working out the details
+No, but I’m joining another dreamer’s project
+No, but I will make myself useful in other ways (Clown Police, etc.)
+Dreams? What´s that? I’ll read all about it on the dreams-page/in the survival guide', ' ', '2100'), ('796114b1-258b-400d-a424-96ce3b3bdcaf', '2025-02-24 17:43:02.608649+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'what-do-you-need-to-bring-to-the-event-to-be-let-in-by-the-threshold-gate-crew', 'What do you need to bring to the event to be let in by the Threshold gate crew?', 'radio', 'Nothing, everybody in the world knows who I am already
+A valid ID and my membership QR code (downloaded or printed in advance)
+What? I thought we are free souls and shouldn't need any of that stuff. I'll double-check the survival guide to make sure I have everything I need when arriving at the gate.', ' ', '2900'), ('86b1d3b2-c317-4f54-92da-5158a8b6acf3', '2025-02-24 17:41:32.102511+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'leaving-no-trace-is-an-important-borderland-principle-what-does-it-mean', 'Leaving No Trace is an important Borderland principle. What does it mean?	', 'radio', ' I will pack everything I brought in and take it out with me, including my rubbish and recycling.	
+ I will pick up and take care of any Matter Out Of Place (MOOP) I see on site, even if I didn't make it.
+ I will not leave MOOP around the site.	
+ I won't poop anywhere but in the toilets.	
+ I will gently remind others to take care of their MOOP if I see it on the site.
+All of the above!', ' ', '2600'), ('88d7cba5-fba7-427e-b61b-53077096ef1e', '2025-02-24 17:32:11.606393+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'whats-your-highest-level-of-completed-education', 'What's your highest level of completed education?', 'radio', 'Bachelor's degree
+Master's degree
+Primary education (up to 10 years)
+Secondary education (10 years or more)
+Technical or vocational certification
+PhD
+No formal education completed
+', ' ', '1500'), ('8bc614ff-8250-4e43-9c48-5f50dc017949', '2025-02-24 17:33:27.870272+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'there-are-a-lot-of-ways-to-engage-in-the-borderland-on-site-and-some-super-important-factions-that-keep-the-event-rolling-to-join-one-of-these-is-also-an-amazing-and-fun-way-to-connect-with-new-people-and-involve-yourself-in-the-community-would-you-like-to-be-part-of-any-of-these-teams-of-heroes', 'There are a lot of ways to engage in the Borderland on site, and some super important factions that keep the event rolling. To join one of these is also an amazing (and fun!) way to connect with new people and involve yourself in the community! Would you like to be part of any of these teams of heroes?', 'checkbox', 'Clown police
+Sanctuary
+Toilets and sanitation
+MOOPicorns
+Consent and awareness
+Threshold (The gate crew with black vests who checks memberships)
+Electricity
+', ' ', '1800'), ('8f852ebe-8d99-4060-a856-1b0e00f942dc', '2025-02-24 17:36:47.018051+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'are-you-part-of-a-camp', 'Are you part of a camp?', 'radio', 'No, I’m free-camping
+Yes, I’m part of a small camp (less than 15 people)
+Yes, I’m part of a big camp (more than 15 people)
+Not yet, but I’m looking for a camp to join (link to camp overview? or camp matchmaking channel on discord?)
+What’s camp? Isn’t that a super cool aesthetic expression?
+', ' ', '2200'), ('92b4d2d2-9b83-4f92-900d-8878b8d1510e', '2025-02-24 17:42:36.764196+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'consent-is-the-unofficial-11th-principle-of-the-borderland-what-does-it-mean', 'Consent is the unofficial 11th principle of the Borderland. What does it mean?	', 'radio', 'Only yes means yes! Not everyone is comfortable saying 'no' directly	
+Consent can be revoked at any time, even if something else was agreed upon before	
+Nudity and sexy costume is not an invitation to touch	
+I'm not owed anything by anyone, and I will gracefully accept a 'no'	
+I can just ask if there's something I want, I can deal with getting a 'no'	
+It's OK for me to check in if I see something that looks non-consensual	
+I'll get permission from everyone in the frame before taking photos
+All of the above!', ' ', '2800'), ('93707ef4-32d9-47a4-9c54-b40ef07e4087', '2025-02-24 17:31:10.447826+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'when-do-you-plan-to-leave', 'When do you plan to leave?', 'radio', 'Before Sunday (but why..?)
+Sunday
+Monday (LNT is sexy!)
+', ' ', '1200'), ('a2e79724-911b-43d7-b51d-b9289668a18b', '2025-02-24 17:35:11.149393+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'clown-police-you-say-clown-police-but-what-does-the-clown-police-do', 'Clown police you say? Clown police! But what does the clown police do?', 'checkbox', 'help to keep everyone safe
+advise and educate
+wear a cute clown costume to be easily identifiable 
+support people in making decisions based on community standards
+be sober, alert and approachable
+be silly and playful
+All of the above!', ' ', '1900'), ('bc19f313-5895-4f4d-aab4-e2dc6adaea4e', '2025-02-24 17:39:06.721716+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'the-borderland-is-informed-by-a-set-of-guidelines-known-as-the-10-11-principles-which-one-is-your-favorite', 'The Borderland is informed by a set of guidelines known as the 10 (11) principles. Which one is your favorite?', 'radio', 'Radical Reliance
+Gifting
+Radical self expression
+Decommodification
+Radical Inclusion
+Communal Effort
+Civic responsibility
+Leave no trace
+Participation
+Immediacy
+Consent
+I haven´t heard about them but will read about it straight away', ' ', '2500'), ('ce88e668-b8da-45f3-8210-eb5b2e214bf0', '2025-02-24 17:31:29.359933+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'how-many-events-based-on-the-10-11-principles-have-you-been-to-before-eg-burning-man-nowhere-the-borderland-', 'How many events based on the 10 (11) principles have you been to before? (e.g. Burning Man, Nowhere, The Borderland, ...)', 'text', ' ', ' ', '1300'), ('e75aac10-de33-4b98-adae-fdd81d924f18', '2025-02-24 17:38:35.211124+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'did-you-know-you-have-to-bring-all-your-own-food-and-drinks-to-the-borderland', 'Did you know you have to bring all your own food and drinks to the Borderland?', 'radio', 'Yes, and I'll bring my own fun too! I'm all on board with radical self-reliance!
+No, but I'll read about it in the survival guide right away!', ' ', '2400'), ('e9ee01bf-06ea-4318-aa5c-0df7ca14718f', '2025-02-24 17:35:47.915476+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'sounds-great-doesnt-it-can-anyone-with-a-membership-become-a-clown-police', 'Sounds great, doesn´t it? Can anyone with a membership become a clown police?', 'radio', 'No, only veterans.
+Yes, it´s a great way to be part of the Borderland!
+Yes, even me! I will sign up for my shift here', ' ', '2000'), ('eb884f8e-e791-49b9-b328-e4b13c71a544', '2025-02-24 17:32:59.924267+00', 'edf815c8-d17d-440c-b2ba-e84a28b8f070', 'who-is-responsible-for-the-borderland-to-happen', 'Who is responsible for the Borderland to happen?', 'checkbox', 'All members of the Borderland are doing it together!
+Margrethe II of Denmark (that's why we have sponsored ballet classes)
+ChatGPT
+That guy *pointing*
+Vito Corleone and the family
+Probably the one who created this endless questionnaire
+', ' ', '1700');
