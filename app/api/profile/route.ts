9d3ab9f38c96438d414eeec75e7728c56ad60fcc
henophilia <tech@henophilia.org>
2025-03-06 06:27:29 +0100

    fix: only send Stripe keys to frontend for admins

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,9 +1,11 @@
 import { requestWithAuth } from "@/app/api/_common/endpoints";
 
 export const GET = requestWithAuth(async (supabase, profile) => {
-  for (const p of profile.projects) {
-    delete (p.burn_config as any).stripe_secret_api_key;
-    delete (p.burn_config as any).stripe_webhook_secret;
+  if (!profile.is_admin) {
+    for (const p of profile.projects) {
+      delete (p.burn_config as any).stripe_secret_api_key;
+      delete (p.burn_config as any).stripe_webhook_secret;
+    }
   }
 
   return profile;
