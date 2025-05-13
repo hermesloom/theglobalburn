917c3891d5599eab79f6750d0d03df15751e4b3d
henophilia <tech@henophilia.org>
2025-05-13 10:26:09 +0200

    feat: handle excluded individuals in set-membership-purchase-right-details

--- app/api/burn/[slug]/set-membership-purchase-right-details/route.ts
+++ app/api/burn/[slug]/set-membership-purchase-right-details/route.ts
@@ -2,12 +2,21 @@ import { requestWithProject, query } from "@/app/api/_common/endpoints";
 import { s } from "ajv-ts";
 import { BurnRole } from "@/utils/types";
 import { checkNoSuchMembershipOrPurchaseRightExists } from "@/app/api/_common/profile";
+import { sendEmail } from "../../../../_components/email";
 
 const SetMembershipPurchaseRightDetailsRequestSchema = s.object({
   first_name: s.string(),
   last_name: s.string(),
   birthdate: s.string().pattern(/^\d{4}-\d{2}-\d{2}$/),
 });
+
+type ExcludedIndividual = Partial<
+  s.infer<typeof SetMembershipPurchaseRightDetailsRequestSchema>
+>;
+
+const excludedIndividuals: ExcludedIndividual[] = JSON.parse(
+  process.env.EXCLUDED_INDIVIDUALS ?? "[]",
+);
 
 export const PATCH = requestWithProject<
   s.infer<typeof SetMembershipPurchaseRightDetailsRequestSchema>
@@ -26,8 +35,31 @@ export const PATCH = requestWithProject<
       project!.id,
       body.first_name,
       body.last_name,
-      body.birthdate
+      body.birthdate,
     );
+
+    if (
+      excludedIndividuals.some((excludedIndividual) =>
+        (Object.keys(excludedIndividual) as (keyof ExcludedIndividual)[]).every(
+          (k) =>
+            excludedIndividual[k] === undefined ||
+            body[k].toLowerCase() === excludedIndividual[k].toLowerCase(),
+        ),
+      )
+    ) {
+      await sendEmail(
+        process.env.BOARD_EMAIL!,
+        "Warning: " +
+          body.first_name +
+          " " +
+          body.last_name +
+          " just tried to acquire a membership!",
+        `Dear board,\n\nThis is to inform you that **${body.first_name} ${body.last_name}** has just tried to enter their personal details in the membership platform to acquire a membership, but has been prevented from doing so through an error message.\n\nNote that this individual, registered in the platform as ${profile.email}, currently has an active membership purchase right which can be revoked by an admin. Alternatively, it will automatically expire at ${project.membership_purchase_right.expires_at}.\n\nBest regards\nMembership platform for ${project.name}`,
+      );
+      throw new Error(
+        `You have been formally excluded from ${project.name}. The board has been informed of your attempt to sign up anyway. If this doesn't seem right, please contact support via the email address at the bottom.`,
+      );
+    }
 
     await query(() =>
       supabase
@@ -38,9 +70,9 @@ export const PATCH = requestWithProject<
           birthdate: body.birthdate,
           details_modifiable: false,
         })
-        .eq("id", project!.membership_purchase_right!.id)
+        .eq("id", project!.membership_purchase_right!.id),
     );
   },
   SetMembershipPurchaseRightDetailsRequestSchema,
-  BurnRole.Participant
+  BurnRole.Participant,
 );
