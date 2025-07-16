a8938f856749d2d81f962d04c63bd64487967638
Brian Underwood <public@brian-underwood.codes>
2025-07-16 17:56:12 +0200

    fix: Threshold phrase

--- app/api/burn/[slug]/admin/pet_search/[chip_code]/route.ts
+++ app/api/burn/[slug]/admin/pet_search/[chip_code]/route.ts
@@ -27,7 +27,7 @@ export const GET = requestWithProject(
     const ids = all_memberships.filter((m: BurnMembership) =>
       Array.isArray(m.metadata?.pets) &&
       m.metadata?.pets.length > 0 &&
-      (chipCode == "all hail the jort" ?
+      (chipCode == "all%20hail%20the%20jort" ?
         true :
         m.metadata.pets.some((pet: Pet) => pet.chip_code === chipCode)
       )
