939c48add39064051b517e6c5f94f46789452685
henophilia <tech@henophilia.org>
2025-11-16 23:25:50 +0100

    feat: add time zone to formatDate output

--- app/burn/[slug]/membership/components/helpers/date.ts
+++ app/burn/[slug]/membership/components/helpers/date.ts
@@ -17,39 +17,55 @@ function formatDate(date: string | number | Date): string {
   const minutes = String(dateObj.getMinutes()).padStart(2, "0");
   const seconds = String(dateObj.getSeconds()).padStart(2, "0");
 
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  // Get timezone abbreviation
+  const timeZoneAbbr =
+    new Intl.DateTimeFormat("en-US", {
+      timeZoneName: "shortGeneric",
+    })
+      .formatToParts(dateObj)
+      .find((part) => part.type === "timeZoneName")?.value || "";
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${timeZoneAbbr}`;
 }
 
 const formatRelativeDateTime = (date: Date) => {
   const now = new Date();
 
-  const diffDays = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
+  const diffDays = Math.floor(
+    (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24),
+  );
 
-  const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const timeString = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
   let relativeDay;
 
   if (diffDays === 0 && now.getDate() === date.getDate()) {
-    relativeDay = 'Today'
+    relativeDay = "Today";
   } else if (diffDays <= 1 && now.getDate() - date.getDate() === 1) {
-    relativeDay = 'Yesterday';
+    relativeDay = "Yesterday";
   } else if (diffDays < 7) {
     relativeDay = `${diffDays === 1 ? "1 day" : `${diffDays} days`} ago`;
   }
 
-  let weekday = date.toLocaleDateString('en-US', { weekday: 'long' });
+  let weekday = date.toLocaleDateString("en-US", { weekday: "long" });
 
   return `${weekday} at ${timeString} (${relativeDay})`;
-}
+};
 
-function formatDOB(dobString: string, highlightUnderage: boolean = false): string {
+function formatDOB(
+  dobString: string,
+  highlightUnderage: boolean = false,
+): string {
   let dob = new Date(dobString);
 
-  return(dob.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }));
+  return dob.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 }
 
 const calculateAge = (birthDate: Date) => {
@@ -68,14 +84,18 @@ const calculateAge = (birthDate: Date) => {
   }
 
   return age;
-}
+};
 
 function isSameDay(date1: Date, date2: Date) {
   return (
-    date1.getMonth() === date2.getMonth() &&
-    date1.getDate() === date2.getDate()
+    date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate()
   );
 }
 
-export { formatDate, formatRelativeDateTime, formatDOB, calculateAge, isSameDay };
-
+export {
+  formatDate,
+  formatRelativeDateTime,
+  formatDOB,
+  calculateAge,
+  isSameDay,
+};
