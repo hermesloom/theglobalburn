f7c65f0d52baaaf4d60e95374a6dbd07febe3ec6
Brian Underwood <public@brian-underwood.codes>
2026-02-15 11:28:18 +0100

    fix: https 🤦‍♂️

--- app/burn/[slug]/rea/page.tsx
+++ app/burn/[slug]/rea/page.tsx
@@ -4,7 +4,7 @@ export default function REAPage() {
   return (
     <div className="-m-14 w-[calc(100%+7rem)] h-[calc(100vh)]">
       <iframe
-        src="http://rea.theborderland.se"
+        src="https://rea.theborderland.se"
         className="w-full h-full border-0"
         allow="fullscreen"
       />
