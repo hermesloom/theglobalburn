905b42631bd7c09cbd5ba96392176138d3d041d9
Brian Underwood <public@brian-underwood.codes>
2026-02-15 14:25:34 +0100

    fix: Change REA tab name to "Volunteer" because "Realities Employment Agency" is too long (and it will probably confuse people anyway)

--- app/burn/[slug]/layout.tsx
+++ app/burn/[slug]/layout.tsx
@@ -48,27 +48,27 @@ export default function ProjectLayout({
               project.membership || project.membership_purchase_right
                 ? "Your Membership"
                 : project.burn_config.current_stage ===
-                      BurnStage.OpenSaleLotteryEntrantsOnly ||
-                    project.burn_config.current_stage ===
-                      BurnStage.OpenSaleGeneral
+                  BurnStage.OpenSaleLotteryEntrantsOnly ||
+                  project.burn_config.current_stage ===
+                  BurnStage.OpenSaleGeneral
                   ? "Open Membership Sale"
                   : project.burn_config.current_stage ===
-                      BurnStage.OpenSaleNonTransferable
+                    BurnStage.OpenSaleNonTransferable
                     ? "Fall Membership Sale"
                     : "Membership Lottery",
             path: `/burn/${project?.slug}/membership`,
             icon: <IdcardOutlined />,
           },
           project.membership ||
-          project.membership_purchase_right ||
-          project.burn_config.current_stage !==
+            project.membership_purchase_right ||
+            project.burn_config.current_stage !==
             BurnStage.OpenSaleNonTransferable
             ? null
             : {
-                label: "Spring Membership Sale",
-                path: `/burn/${project?.slug}/spring-membership-info`,
-                icon: <IdcardOutlined />,
-              },
+              label: "Spring Membership Sale",
+              path: `/burn/${project?.slug}/spring-membership-info`,
+              icon: <IdcardOutlined />,
+            },
           {
             label: "Links",
             path: `/burn/${project?.slug}/links`,
@@ -76,10 +76,10 @@ export default function ProjectLayout({
           },
           project.membership
             ? {
-                label: "Statistics",
-                path: `/burn/${project?.slug}/statistics`,
-                icon: <BarChartOutlined />,
-              }
+              label: "Statistics",
+              path: `/burn/${project?.slug}/statistics`,
+              icon: <BarChartOutlined />,
+            }
             : null,
           {
             label: "Newsletter",
@@ -88,85 +88,85 @@ export default function ProjectLayout({
           },
           profile?.email === "ml@semi-sentient.com"
             ? {
-                label: "Realities Employment Agency",
-                path: `/burn/${project?.slug}/rea`,
-                icon: <RocketOutlined />,
-              }
+              label: "Volunteer",
+              path: `/burn/${project?.slug}/rea`,
+              icon: <RocketOutlined />,
+            }
             : null,
           project.membership
             ? {
-                label: "Have an idea?",
-                path: `/burn/${project?.slug}/ideas`,
-                icon: <PlusOutlined />,
-              }
+              label: "Have an idea?",
+              path: `/burn/${project?.slug}/ideas`,
+              icon: <PlusOutlined />,
+            }
             : null,
 
           ...(project.roles.includes(BurnRole.MembershipScanner) ||
-          project.roles.includes(BurnRole.ThresholdWatcher)
+            project.roles.includes(BurnRole.ThresholdWatcher)
             ? ([{ separator: true }, { sectionTitle: "On-site" }] as any)
             : []),
 
           ...(project.roles.includes(BurnRole.MembershipScanner)
             ? ([
-                {
-                  label: "Membership scanner",
-                  path: `/burn/${project?.slug}/scanner`,
-                  icon: <QrcodeOutlined />,
-                },
-              ] as any)
+              {
+                label: "Membership scanner",
+                path: `/burn/${project?.slug}/scanner`,
+                icon: <QrcodeOutlined />,
+              },
+            ] as any)
             : []),
 
           ...(project.roles.includes(BurnRole.ThresholdWatcher)
             ? ([
-                {
-                  label: "Watcher Tools",
-                  path: `/burn/${project?.slug}/watcher_tools`,
-                  icon: <MonitorOutlined />,
-                },
-                {
-                  label: "Pet search",
-                  path: `/burn/${project?.slug}/pet_search`,
-                  icon: <BaiduOutlined />,
-                },
-              ] as any)
+              {
+                label: "Watcher Tools",
+                path: `/burn/${project?.slug}/watcher_tools`,
+                icon: <MonitorOutlined />,
+              },
+              {
+                label: "Pet search",
+                path: `/burn/${project?.slug}/pet_search`,
+                icon: <BaiduOutlined />,
+              },
+            ] as any)
             : []),
 
           ...(project.roles.includes(BurnRole.MembershipManager)
             ? ([
-                { separator: true },
-                { sectionTitle: "Membership management" },
-                project.burn_config.current_stage !==
-                  BurnStage.OpenSaleLotteryEntrantsOnly &&
+              { separator: true },
+              { sectionTitle: "Membership management" },
+              project.burn_config.current_stage !==
+                BurnStage.OpenSaleLotteryEntrantsOnly &&
                 project.burn_config.current_stage !== BurnStage.OpenSaleGeneral
-                  ? {
-                      label: "Lottery tickets",
-                      path: `/burn/${project?.slug}/admin/lottery-tickets`,
-                      icon: <WalletOutlined />,
-                    }
-                  : null,
-                {
-                  label: "Membership purchase rights",
-                  path: `/burn/${project?.slug}/admin/membership-purchase-rights`,
-                  icon: <FileDoneOutlined />,
-                },
-                {
-                  label: "Memberships",
-                  path: `/burn/${project?.slug}/admin/memberships`,
-                  icon: <TeamOutlined />,
-                },
-              ] as any)
+                ? {
+                  label: "Lottery tickets",
+                  path: `/burn/${project?.slug}/admin/lottery-tickets`,
+                  icon: <WalletOutlined />,
+                }
+                : null,
+              {
+                label: "Membership purchase rights",
+                path: `/burn/${project?.slug}/admin/membership-purchase-rights`,
+                icon: <FileDoneOutlined />,
+              },
+              {
+                label: "Memberships",
+                path: `/burn/${project?.slug}/admin/memberships`,
+                icon: <TeamOutlined />,
+              },
+            ] as any)
             : []),
 
           ...(project.roles.includes(BurnRole.Admin)
             ? ([
-                { separator: true },
-                { sectionTitle: "Administration" },
-                {
-                  label: "Configuration",
-                  path: `/burn/${project?.slug}/admin/config`,
-                  icon: <SettingOutlined />,
-                },
-              ] as any)
+              { separator: true },
+              { sectionTitle: "Administration" },
+              {
+                label: "Configuration",
+                path: `/burn/${project?.slug}/admin/config`,
+                icon: <SettingOutlined />,
+              },
+            ] as any)
             : []),
           ,
         ].filter((x) => !!x)}
