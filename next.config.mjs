9508bd2768ff2b72fc4e49434b3e87696925d35d
Brian Underwood <public@brian-underwood.codes>
2026-03-09 10:38:39 +0100

    fix: Allow cross-origin iframe in chrome

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "credentialless",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-        ],
-      },
-      {
         source: "/api/:path*",
         headers: [
           {
