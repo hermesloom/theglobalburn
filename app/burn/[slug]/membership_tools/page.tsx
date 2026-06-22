00553d06826836033687280bc403d2ce8a6536d3
Brian Underwood <public@brian-underwood.codes>
2026-06-22 15:42:22 -0400

    feat: Make phone numbers in membershp tools clickable

--- app/burn/[slug]/membership_tools/page.tsx
+++ app/burn/[slug]/membership_tools/page.tsx
@@ -323,7 +323,7 @@ export default function ScannerManagerPage() {
                             <div key="emergency">
                               <h3 className="text-lg font-semibold mt-1">Emergency Info</h3>
                               {membership.metadata.camp_name && <p><strong>Camp:</strong> {membership.metadata.camp_name}</p>}
-                              {membership.metadata.phone_number && <p><strong>Phone:</strong> {membership.metadata.phone_number}</p>}
+                              {membership.metadata.phone_number && <p><strong>Phone:</strong> <a href={`tel:${membership.metadata.phone_number}`}>{membership.metadata.phone_number}</a></p>}
                               {membership.metadata.emergency_contact_onsite && <p><strong>On-site contact:</strong> {membership.metadata.emergency_contact_onsite}</p>}
                               {membership.metadata.emergency_contact_other && <p><strong>Other contact:</strong> {membership.metadata.emergency_contact_other}</p>}
                             </div>
@@ -334,7 +334,7 @@ export default function ScannerManagerPage() {
                               <h3 className="text-lg font-semibold mt-1">Sleeper Vehicle</h3>
                               {membership.metadata.car_registration.registration_plate && <p><strong>Plate:</strong> {membership.metadata.car_registration.registration_plate}</p>}
                               {membership.metadata.car_registration.camp_or_area && <p><strong>Camp/Area:</strong> {membership.metadata.car_registration.camp_or_area}</p>}
-                              {membership.metadata.car_registration.phone_number && <p><strong>Phone:</strong> {membership.metadata.car_registration.phone_number}</p>}
+                              {membership.metadata.car_registration.phone_number && <p><strong>Phone:</strong> <a href={`tel:${membership.metadata.car_registration.phone_number}`}>{membership.metadata.car_registration.phone_number}</a></p>}
                               {membership.metadata.car_registration.alt_contact && <p><strong>Alt contact:</strong> {membership.metadata.car_registration.alt_contact}</p>}
                             </div>
                           )}
